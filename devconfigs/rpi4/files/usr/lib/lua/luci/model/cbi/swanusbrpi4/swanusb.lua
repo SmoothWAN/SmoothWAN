075fa090205485899d188639573b17fd4235f860
TalalMash <0talal.mash0@gmail.com>
2022-03-12 10:46:32 -0800

    Remove NCM from swanusbrpi4 and cleanup

--- devconfigs/rpi4/files/usr/lib/lua/luci/model/cbi/swanusbrpi4/swanusb.lua
+++ devconfigs/rpi4/files/usr/lib/lua/luci/model/cbi/swanusbrpi4/swanusb.lua
@@ -3,7 +3,7 @@ local net = require "luci.model.network".init()
 local sys = require "luci.sys"
 local ifaces = sys.net:devices()
 
-m = Map("network", translate("Multi-WAN USB Setup"), translate("• Note: Enter empty name to all ports to use 'Multi-WAN MAC'<br> <img src='\\luci-static/bootstrap/mwanusb.svg' width = 400>"))
+m = Map("network", translate("Multi-WAN USB Setup"), translate("• Note: Empty the names to use 'Multi-WAN MAC'<br> <img src='\\luci-static/bootstrap/mwanusb.svg' width = 400>"))
 
 s = m:section(TypedSection, "interface", "USB Ports")
 
@@ -13,12 +13,16 @@ function s:filter(value)
      return value == "One" or value == "Two" or value == "Three" or value == "Four"
 end
 
-n = s:option(Value, "usename", translate("Name in Speedify:"), translate("Enter no more than 10 characters containing numbers, letters and '-' only"))
+n = s:option( Value, "usename", translate("Name in Speedify:"), translate("Up to 10 characters containing numbers, letters and '-' only"))
+
+n.write = function(self, section, value)
+        self.map:set(section, "device", value)
+        return Value.write(self, section, value)
+end
 
 p = s:option(ListValue, "proto", "Protocol:")
 p:value("dhcp", "DHCP")
 p:value("static", "Static")
-p:value("ncm", "NCM")
 
 s:option(Value, "ipaddr", translate("IP Address:")):depends("proto", "static")
 nm = s:option(Value, "netmask", translate("Netmask:"))
@@ -26,43 +30,7 @@ nm:depends("proto", "static")
 nm.default = '255.255.255.0'
 s:option(Value, "gateway", translate("Gateway:")):depends("proto", "static")
 
-cm = s:option(Value, "device", translate("NCM Device:"), translate("'/dev/cdc-wdm0' is commonly used. Increment the last number if more than one NCM device is used."))
-cm:depends("proto", "ncm")
-cm.default = '/dev/cdc-wdm0'
-
-cmsvc = s:option(ListValue, "service", translate("Service Type:"))
-cmsvc:depends("proto", "ncm")
-cmsvc:value("preferlte","Prefer LTE")
-cmsvc:value("preferumts","Prefer UMTS")
-cmsvc:value("lte","LTE only")
-cmsvc:value("umts","UMTS/GPRS")
-cmsvc:value("gsm","GPRS only")
-cmsvc:value("auto","Automatic")
-
-pdptyp = s:option(ListValue, "pdptype", translate("IP Protocol:"))
-pdptyp:depends("proto","ncm")
-pdptyp:value("IP","IPv4")
-pdptyp:value("IPV4V6","IPv4+IPv6")
-pdptyp:value("IPV6","IPv6")
-
-apn = s:option(Value, "apn", translate("APN:"))
-apn:depends("proto","ncm")
-
-pincod = s:option(Value, "pincode", translate("Pin code:"))
-pincod:depends("proto","ncm")
-
-usernam = s:option(Value, "username", translate("Username:"))
-usernam:depends("proto","ncm")
-
-passw = s:option(Value, "password", translate("Passowrd:"))
-passw:depends("proto","ncm")
-passw.password = true
-
-dialno = s:option(Value, "dialnumber", translate("Dial Number:"))
-dialno:depends("proto","ncm")
-
-mtt = s:option(Value, "metric", translate("Bypass priority:"), translate("If Speedify is down or disabled, the WAN with the lower number will be used, set all WANs to the same number for no particular order. Number starts at 10"))
+mtt = s:option(Value, "metric", translate("Bypass priority:"), translate("The WAN with the lowest number will be used when Speedify is disconnected."))
 mtt.default = '10'
 
 return m
-
--- devconfigs/rpi4/files/usr/share/smoothwan/helpers/autousb.sh
+++ devconfigs/rpi4/files/usr/share/smoothwan/helpers/autousb.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+renamedev () {
+	ip link set $1 down
+	ip link set $1 name $2
+	ip link set $2 up
+}
+
 while :
 do
 
@@ -8,66 +14,44 @@ USB2name="$(uci get network.Two.usename)"
 USB3name="$(uci get network.Three.usename)"
 USB4name="$(uci get network.Four.usename)"
 
-USB13="$(ls -l /sys/bus/usb/devices/2-1:1.0/net | awk -F ' ' '{print $9}')"
-USB23="$(ls -l /sys/bus/usb/devices/2-2:1.0/net | awk -F ' ' '{print $9}')"
-USB1="$(ls -l /sys/bus/usb/devices/1-1.1:1.0/net | awk -F ' ' '{print $9}')"
-USB2="$(ls -l /sys/bus/usb/devices/1-1.2:1.0/net | awk -F ' ' '{print $9}')"
-USB3="$(ls -l /sys/bus/usb/devices/1-1.3:1.0/net | awk -F ' ' '{print $9}')"
-USB4="$(ls -l /sys/bus/usb/devices/1-1.4:1.0/net | awk -F ' ' '{print $9}')"
+#not using functions to concatenate different hubs
+PATH13="/sys/bus/usb/devices/2-1:1.0"
+PATH23="/sys/bus/usb/devices/2-2:1.0"
+PATH1="/sys/bus/usb/devices/1-1.1:1.0"
+PATH2="/sys/bus/usb/devices/1-1.2:1.0"
+PATH3="/sys/bus/usb/devices/1-1.3:1.0"
+PATH4="/sys/bus/usb/devices/1-1.4:1.0"
+USB13="$(ls -l $PATH13/net | awk -F ' ' '{print $9}')"
+USB23="$(ls -l $PATH23/net | awk -F ' ' '{print $9}')"
+USB1="$(ls -l $PATH1/net | awk -F ' ' '{print $9}')"
+USB2="$(ls -l $PATH2/net | awk -F ' ' '{print $9}')"
+USB3="$(ls -l $PATH3/net | awk -F ' ' '{print $9}')"
+USB4="$(ls -l $PATH4/net | awk -F ' ' '{print $9}')"
 
 if [[ ${USB13} ]] && [[ ${USB13} != ${USB1name} ]]; then
-		ip link set ${USB13} down
-		ip link set ${USB13} name ${USB1name}
-		ip link set ${USB1name} up
-		ifup ${USB1name}
-		uci set network.One.device=${USB1name}
-		uci commit network
-		logger -t swan "Renamed USB1 (3.0+) to ${USB1name}"
+		renamedev "${USB13}" "${USB1name}"
+		logger -t mwanusb "Renamed USB1 (3.0+) to ${USB1name}"
 	fi
 if [[ ${USB23} ]] && [[ ${USB23} != ${USB2name} ]]; then
-		ip link set ${USB23} down
-		ip link set ${USB23} name ${USB2name}
-		ip link set ${USB2name} up
-		ifup ${USB2name}
-		uci set network.Two.device=${USB2name}
-		uci commit network
-		logger -t swan "Renamed USB2 (3.0+) to ${USB2name}"
+		renamedev "${USB23}" "${USB2name}"
+		logger -t mwanusb "Renamed USB2 (3.0+) to ${USB2name}"
 	fi
 if [[ ${USB1} ]] && [[ ${USB1} != ${USB1name} ]]; then
-		ip link set ${USB1} down
-		ip link set ${USB1} name ${USB1name}
-		ip link set ${USB1name} up
-		ifup ${USB1name}
-		uci set network.One.device=${USB1name}
-                uci commit network
-		logger -t swan "Renamed USB1 to ${USB1name}"
+		renamedev "${USB1}" "${USB1name}"
+		logger -t mwanusb "Renamed USB1 to ${USB1name}"
 	fi
 if [[ ${USB2} ]] && [[ ${USB2} != ${USB2name} ]]; then
-		ip link set ${USB2} down
-		ip link set ${USB2} name ${USB2name}
-		ip link set ${USB2name} up
-		ifup ${USB2name}
-		uci set network.Two.device=${USB2name}
-                uci commit network
-		logger -t swan "Renamed USB2 to ${USB2name}"
+		renamedev "${USB2}" "${USB2name}"
+		logger -t mwanusb "Renamed USB2 to ${USB2name}"
 	fi
 if [[ ${USB3} ]] && [[ ${USB3} != ${USB3name} ]]; then
-		ip link set ${USB3} down
-		ip link set ${USB3} name ${USB3name}
-		ip link set ${USB3name} up
-		ifup ${USB3name}
+		renamedev "${USB3}" "${USB3name}"
 		uci set network.Three.device=${USB3name}
-                uci commit network
-		logger -t swan "Renamed USB3 to ${USB3name}"
+		logger -t mwanusb "Renamed USB3 to ${USB3name}"
 	fi
 if [[ ${USB4} ]] && [[ ${USB4} != ${USB4name} ]]; then
-		ip link set ${USB4} down
-		ip link set ${USB4} name ${USB4name}
-		ip link set ${USB4name} up
-		uci set network.Four.device=${USB4name}
-                uci commit network
-		ifup ${USB4name}
-		logger -t swan "Renamed USB4 to ${USB4name}"
+		renamedev "${USB4}" "${USB4name}"
+		logger -t mwanusb "Renamed USB4 to ${USB4name}"
 	fi
 
 sleep 5
