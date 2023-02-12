7eb0089122e7b5dafe4521723e8d65fa3b70904e
TalalMash <0talal.mash0@gmail.com>
2023-02-12 16:50:49 -0600

    Update pkgs, sync platforms, remove swanmacaddr

--- devconfigs/flint/sideload-glibc.sh
+++ devconfigs/flint/sideload-glibc.sh
@@ -5,4 +5,10 @@ wget http://ftp.de.debian.org/debian/pool/main/g/glibc/libc6_2.31-13+deb11u5_arm
 ar x *.deb
 tar -xvf data.tar.xz
 cp lib usr ../files/ -rP
-cd ..
+mkdir gcc
+cd gcc || exit 1
+wget https://deb.debian.org/debian/pool/main/g/gcc-8/libgcc1_8.3.0-6_armhf.deb
+ar x *.deb
+tar -xvf data.tar.xz
+cp lib usr ../../files/ -rP
+cd ../..
