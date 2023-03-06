54fbc114e7c58a43596b1e44170d233b2d2aa034
TalalMash <0talal.mash0@gmail.com>
2023-03-05 19:18:20 -0600

    HF4 unified USB renamer, PBR fixess, disable HTTPS

--- devconfigs/slateax/sideload-glibc.sh
+++ devconfigs/slateax/sideload-glibc.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 mkdir sideload-glibc
 cd sideload-glibc || exit 1
-wget http://ftp.de.debian.org/debian/pool/main/g/glibc/libc6_2.31-13+deb11u5_armhf.deb
+wget -c http://ftp.de.debian.org/debian/pool/main/g/glibc/libc6_2.31-13+deb11u5_armhf.deb
 ar x *.deb
 tar -xvf data.tar.xz
 cp lib usr ../files/ -rP
 mkdir gcc
 cd gcc || exit 1
-wget https://deb.debian.org/debian/pool/main/g/gcc-8/libgcc1_8.3.0-6_armhf.deb
+wget -c https://deb.debian.org/debian/pool/main/g/gcc-8/libgcc1_8.3.0-6_armhf.deb
 ar x *.deb
 tar -xvf data.tar.xz
 cp lib usr ../../files/ -rP
