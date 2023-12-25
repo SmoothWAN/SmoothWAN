2421a303a404971a14bd922e4f5fe9b64af0d9f8
TalalMash <0talal.mash0@gmail.com>
2023-12-24 22:16:09 -0500

    Switch to WPA3-only seleciton in Simple Wi-Fi + iOS notice

--- devconfigs/flint/files/www/luci-static/resources/view/network/wireless_simple.js
+++ devconfigs/flint/files/www/luci-static/resources/view/network/wireless_simple.js
@@ -817,11 +817,11 @@ return view.extend({
 		s.addremove = false;
 
 
-		ms = m.section(form.TypedSection, 'wifi-iface', _('Wireless Setup'), _("- Wi-Fi channel follows wireless WAN if enabled. <br>- Click <b>Scan</b> button above to view available networks.<br>- Click <b>Enable</b> in the interfaces above after entering and applying wireless WAN settings.<br>- This configuration page is dumbed down & no automation to support various devices and hardware."));
+		ms = m.section(form.TypedSection, 'wifi-iface', _('Wireless Setup'), _("- Wi-Fi channel follows wireless WAN if enabled. <br>- Click <b>Scan</b> button above to view available networks.<br>- Click <b>Enable</b> in the interfaces above after entering and applying wireless WAN settings.<br>- This configuration page is dumbed down & no automation to support various devices and hardware.<br>- Use WPA3 encryption in WAN section for iOS Hotspot."));
 		t = ms.option(form.Value, 'ssid' ,	_('SSID'));
 		p = ms.option(form.Value, 'key' ,	_('Password'));
 		t = ms.option(form.ListValue, 'encryption', _('Encryption'));
-		t.value('sae-mixed', _('WPA2/WPA3 Mixed'));
+		t.value('sae', _('WPA3'));
 		t.value('psk2', _('WPA2'));
 		t.value('owe', _('Open'));
 
